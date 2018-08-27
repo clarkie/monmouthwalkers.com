55ee99710c3b01e94a20a695e586485a05c7d593
Clarkie <andrew.t.clarke@gmail.com>
2018-08-27 12:11:36 +0100

    change gallery to thumbnails not carousel

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,37 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var pkg = require('./package.json');
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var pkg = require("./package.json");
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
-
+gulp.task("vendor", function() {
   // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+  gulp
+    .src([
+      "./node_modules/bootstrap/dist/**/*",
+      "!./node_modules/bootstrap/dist/css/bootstrap-grid*",
+      "!./node_modules/bootstrap/dist/css/bootstrap-reboot*"
     ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
+    .pipe(gulp.dest("./vendor/bootstrap"));
 
   // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
+  gulp
+    .src([
+      "./node_modules/jquery/dist/*",
+      "!./node_modules/jquery/dist/core.js"
     ])
-    .pipe(gulp.dest('./vendor/jquery'))
+    .pipe(gulp.dest("./vendor/jquery"));
 
-})
+  // jQuery
+  gulp
+    .src(["./node_modules/ekko-lightbox/dist/*"])
+    .pipe(gulp.dest("./vendor/lightbox"));
+});
 
 // Default task
-gulp.task('default', ['vendor']);
+gulp.task("default", ["vendor"]);
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task("browserSync", function() {
   browserSync.init({
     server: {
       baseDir: "./"
@@ -35,7 +40,7 @@ gulp.task('browserSync', function() {
 });
 
 // Dev task
-gulp.task('dev', ['browserSync'], function() {
-  gulp.watch('./css/*.css', browserSync.reload);
-  gulp.watch('./*.html', browserSync.reload);
+gulp.task("dev", ["browserSync"], function() {
+  gulp.watch("./css/*.css", browserSync.reload);
+  gulp.watch("./*.html", browserSync.reload);
 });
